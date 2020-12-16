77a994454e98f0f7bd4b066e752e966114c782cd
Lucas King <sonicluc03@gmail.com>
2020-12-16 10:34:15 -0800

    XKCD Downloader Completed

---
+++ webscraping/xkcddownloader.py
@@ -0,0 +1,44 @@
+# xkcddownloader.py
+# automated download of xkcd comics from web
+
+import os
+import requests
+import bs4
+
+# setup the url for xkcd
+url = "http://xkcd.com"
+
+# create a folder to store the comics
+os.makedirs("xkcd_images", exist_ok=True)
+
+# loop to download all the comics
+while not url.endswith("#"):
+    # download the page (html)
+    print(f"Downloading page {url}...")
+    res = requests.get(url)
+    res.raise_for_status()
+    soup = bs4.BeautifulSoup(res.text,  "html.parser")
+
+    # find the URL of picture using bs4
+    comic_elem = soup.select("#comic img") # list
+
+    if comic_elem == []:
+        print("\tCan't find the image...")
+    else:
+        if comic_elem[0].get("src").startswith("/2067/"):
+            comic_url = "https://xkcd.com" + comic_elem[0].get("src")
+        else:
+            comic_url = "https:" + comic_elem[0].get("src")
+
+        # download the image
+        res = requests.get(comic_url)
+        print(f"\tDownloading image at {comic_url}...")
+
+        # save the image
+        image_file = open(os.path.join("xkcd_images", os.path.basename(comic_url)), "wb")
+        for chunk in res.iter_content(1000000):
+            image_file.write(chunk)
+
+    # get the previous button URL
+    prev_link = soup.select('a[rel="prev"]')[0] #  "/
+    url = "https://xkcd.com" + prev_link.get("href")
